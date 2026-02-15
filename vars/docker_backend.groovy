c17dde105ba296243c7f16991daf72aafb73b314
Anvekarshashi <164373642+Anvekarshashi@users.noreply.github.com>
2026-02-15 14:00:26 +0530

    Update docker_backend.groovy

--- vars/docker_backend.groovy
+++ vars/docker_backend.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectName, String ImgaeTag, String DockerHubUser) {
    echo "Building backend Docker images..."
-  sh "docker build -t anvekarshashi/backend:latest ."
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag ."
   echo "Docker backend image built."
 }

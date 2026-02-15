7cf4a1a1647476d8ac9d86f878306fb20ac172c6
Anvekarshashi <164373642+Anvekarshashi@users.noreply.github.com>
2026-02-15 13:11:55 +0530

    Update and rename docker_build.groovy to docker_backend.groovy

--- vars/docker_backend.groovy
+++ vars/docker_backend.groovy
@@ -1,3 +1,5 @@
 def call(String ProjectName, String ImgaeTag, String DockerHubUser) {
+   echo "Building backend Docker images..."
   sh "docker build -t anvekarshashi/backend:latest ."
+  echo "Docker backend image built."
 }
